f69e58867181291cecedf453f4c61a4b507f4a3e
gapiapja <82304679+gapiapja@users.noreply.github.com>
2024-02-09 14:19:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r Utama
-    git clone https://github.com/anamnich/Utama.git
+    git clone https://github.com/gapiapja/theme.git
     cd Utama
     rm /var/www/pterodactyl/resources/scripts/Utama.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
